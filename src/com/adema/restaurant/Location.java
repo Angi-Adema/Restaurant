6fc26de80539914f2509ad25438a94046ad4df51
Angi-Adema <dr.adema@gmail.com>
2026-01-13 16:43:30 -0700

    added pseudocode and sourcecode.

--- src/com/adema/restaurant/Location.java
+++ src/com/adema/restaurant/Location.java
@@ -4,7 +4,32 @@ public class Location {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		// PSEUDOCODE
+		// 1. Create variables holding: 
+		//      restaurantName, 
+		//      businessAddress, 
+		//      city, 
+		//      state, 
+		//      zip
+		// 2. Print restaurant name moving cursor to next line.
+		// 3. Print business address moving cursor to next line.
+		// 4. Print city, state and zip moving cursor to next line.
+		
+		// Variables
+		String restaurantName = "Flemmings";
+		String businessAddress = "191 Inverness Drive West";
+		String city = "Englewood";
+		String state = "CO";
+		int zip = 80112;
+		
+		// Restaurant name print statement.
+		System.out.println(restaurantName);
+		
+		// Business address print statement.
+		System.out.println(businessAddress);
+		
+		// Print statement for city, state and zip.
+		System.out.println(city + ", " + state + ", " + zip);
 		
 	}
 }
